2d343646901813b6926081398b4b2a9c4f1e08b5
canudooo <129288327+canudooo@users.noreply.github.com>
2023-08-24 08:59:20 -0300

    Add files via upload

---
+++ Linguagem C/Ex 21 da Lista de Ferias.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int p1,p2,p3,p4,p5,nota;
+int i[5];
+
+int main()
+{
+    for(i=0; i<=4; i++)
+    {
+        printf("Insira sua nota na prova I até V, em ordem crescente:\n ");
+        scanf("%d",& p[i]);
+        i++
+    }
+    switch
+{
+case (p[0]>70) && (p[1]>70) && (p[2]>70) && (p[3]>70) && (p[4]>70);
+        priftf("A");
+    case (p[0]>70) && (p[1]>70) && (p[2]>70) && (p[3]>70) || (p[4]>70);
+        priftf("B");
+    case (p[0]>70) && (p[1]>70) && (p[2]>70) || (p[3]>70) && (p[4]>70);
+        priftf("C");
+        default
+        priftf("REPROVADO!");
+    }
+
+    return 0;
+}
