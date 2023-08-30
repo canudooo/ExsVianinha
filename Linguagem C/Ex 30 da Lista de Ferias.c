87d01400c4129f8876b71a679eee42e522c579cb
canudooo <129288327+canudooo@users.noreply.github.com>
2023-08-30 19:08:27 -0300

    Update Ex 30 da Lista de Ferias.c

--- Linguagem C/Ex 30 da Lista de Ferias.c
+++ Linguagem C/Ex 30 da Lista de Ferias.c
@@ -4,13 +4,15 @@
 int main()
 {
 
-int i,soma;
-
+    int soma,i;
+i=0;
 soma=0;
-    for (i=0;i<=99;i++){
-        printf("%d",& i );
-        (soma=soma+i);
-    }
- printf("Soma:%d",& soma) ;
 
+    for(i=0;i<=100;i++){
+        printf("\n%d", i);
+        soma=soma+i;
+    }
+    printf("\n soma:%d",soma);
+
+return 0;
 }
