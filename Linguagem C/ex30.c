a874c51cdb95a89ba1c83b6dfc31c3eb56fc8267
canudooo <129288327+canudooo@users.noreply.github.com>
2023-08-30 18:21:33 -0300

    Add files via upload

---
+++ Linguagem C/ex30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int i,soma;
+
+soma=0;
+    for (i=0;i<=99;i++){
+        printf("%d",& i );
+        (soma=soma+i);
+    }
+ printf("Soma:%d",& soma) ;
+
+}
